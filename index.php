f00b51d683eba0a27847d1a0ed5a61d6f23cc61c
Liudas Bagdonas <liudasbagd@gmail.com>
2020-10-19 11:39:12 +0300

    kaciu apvaisinimo double for loop

--- index.php
+++ index.php
@@ -1,2 +1,25 @@
 <?php
-print 'ads';
+$kates = rand(1,3);
+$sunys = rand(1,3);
+$katasuniai = 0;
+
+for($i = 1; $i <= $kates; $i++){
+    for($j = 1; $j <= $sunys; $j++){
+        if(rand(0,1)){
+            $katasuniai++;
+            break;
+        }
+    }
+}
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ciklai</title>
+</head>
+<body>
+    <h1>Katasuniu Iseiga</h1>
+    <h2>Dalyvavo <?php print $kates ?> kates ir <?php print $sunys ?> sunys</h2>
+    <h3>Katasuniu iseiga: <?php print $katasuniai ?></h3>
+</body>
+</html>
