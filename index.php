27761fb31b2a0a359556568e08ef6b592c07662d
Liudas Bagdonas <liudasbagd@gmail.com>
2020-10-19 14:32:40 +0300

    square of *

--- index.php
+++ index.php
@@ -1,28 +1,25 @@
 <?php
-$months = 12;
-$wallet = 1000;
-$month_income = 700;
-$h2 = '';
+$stars_count = rand(5,80);
+$line = '';
 
-for($i = 1; $i <= $months; $i++){
-    $month_expenses = rand(300, 1100);
-    $wallet += $month_income;
-    $wallet -= $month_expenses;
-    $h2 = "Po $months m., prognozuojamas likutis $wallet.";
-
-    if($wallet <= 0) {
-        $h2 = "Atsargiai, $i menesi gali baigtis pinigai.";
-        break;
+for($i = 0; $i <= $stars_count; $i++){
+    for($j = 0; $j <= $stars_count; $j++){
+        if($j <= $stars_count){
+            $line .= '*';
+        }
     }
+    $line .= '<br>';
 }
 ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Ciklai</title>
+    <style>
+
+    </style>
 </head>
 <body>
-    <h1>Wallet Forecast</h1>
-    <h2><?php print $h2; ?></h2>
+    <p><?php print $line; ?></p>
 </body>
 </html>
