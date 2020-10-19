d7bbd6734fc613cd3f5c43359428bbb7f23a67f0
Liudas Bagdonas <liudasbagd@gmail.com>
2020-10-19 12:57:37 +0300

    pinigines balanso lyginimas

--- index.php
+++ index.php
@@ -2,14 +2,19 @@
 $months = 12;
 $wallet = 1000;
 $month_income = 700;
-
+$h2 = '';
 
 for($i = 1; $i <= $months; $i++){
+    $month_expenses = rand(300, 1100);
     $wallet += $month_income;
-    $wallet -= $month_expenses = rand(300, 1100);
-}
+    $wallet -= $month_expenses;
+    $h2 = "Po $months m., prognozuojamas likutis $wallet.";
 
-$h2 = "Po $months m., prognozuojamas likutis $wallet."
+    if($wallet <= 0) {
+        $h2 = "Atsargiai, $i menesi gali baigtis pinigai.";
+        break;
+    }
+}
 ?>
 <html lang="en">
 <head>
