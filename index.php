b9a2a5943a267384cc5ffdaf7489d394aef9e55f
Liudas Bagdonas <liudasbagd@gmail.com>
2020-10-22 16:29:27 +0300

    created sudoku

--- index.php
+++ index.php
@@ -1,29 +1,14 @@
 <?php
-$users = [
-    [
-        'user_name' => 'black_widow',
-        'password' => 'widdow2',
-        'image' => 'https://qt.azureedge.net/resources/authors-images-large/sasha-grey.jpg',
-        'email' => 'b_widow@gmail.com',
-    ],
-    [
-        'user_name' => 'cap_america',
-        'password' => '1maCap',
-        'image' => 'https://www.edgemedianetwork.com/display/viewimage_storyMain.php?id=297082&minwidth=600&maxwidth=1000',
-        'email' => 'cpt_america@gmail.com',
-    ],
-    [
-        'user_name' => 'gomora',
-        'password' => 'starL000rd',
-        'image' => 'https://media1.s-nbcnews.com/i/newscms/2015_02/835681/150106-mia-khalifa-830a_bcc977bc287eeeb9c3148b332b0e1a7b.jpg',
-        'email' => 'sodoma_gomora@gmail.com',
-    ],
-    [
-        'user_name' => 'star_lord',
-        'password' => 'widdow2',
-        'image' => 'https://timesofindia.indiatimes.com/thumb/msid-59796527,width-1200,height-900,resizemode-4/.jpg',
-        'email' => 'star_lord@gmail.com',
-    ]
+$sudoku =[
+    [8, 2, 7, 9, 6, 5, 4, 3, 1,],
+    [1, 5, 4, 3, 2, 7, 6, 8, 9,],
+    [3, 9, 6, 1, 4, 8, 7, 5, 2,],
+    [5, 9, 3, 4, 7, 2, 6, 1, 8,],
+    [4, 6, 8, 5, 1, 3, 9, 7, 2,],
+    [2, 7, 1, 6, 8, 9, 4, 3, 5,],
+    [7, 6, 8, 1, 5, 4, 2, 3, 9,],
+    [2, 3, 5, 7, 9, 6, 8, 4, 1,],
+    [9, 1, 4, 8, 2, 3, 5, 6, 7,],
 ];
 ?>
 <html lang="en">
@@ -31,33 +16,42 @@ $users = [
     <meta charset="UTF-8">
     <title>Foreach ciklas</title>
     <style>
-        article  {
-            display: flex;
-        }
-        section {
-            width: 200px;
-            margin: 10px;
-            border: 1px solid black;
+        table {
             text-align: center;
+            margin: 50px auto;
+            width: 500px;
+            height: 500px;
+            border-collapse: collapse;
         }
-        div {
-            height: 200px;
-            background-position: top;
-            background-size: cover;
-            background-repeat: no-repeat;
+        table tr td {
+            border: 1px solid lightgray;
         }
+        td:first-child {
+            border-left: solid;
+        }
+        td:nth-child(3n) {
+            border-right: solid;
+        }
+        tr:first-child {
+            border-top: solid;
+        }
+        tr:nth-child(3n) td {
+            border-bottom: solid;
+        }
+
     </style>
 </head>
 <body>
-    <article>
-        <?php foreach($users as $user): ?>
-            <section>
-                <div style="background-image: url(<?php print $user['image']; ?>)" ></div>
-                <h3>User name: <?php print $user['user_name']; ?></h3>
-                <p>Password: <?php print $user['password']; ?></p>
-                <p>Email: <?php print $user['email']; ?></p>
-            </section>
+    <table>
+        <tbody>
+        <?php foreach($sudoku as $square): ?>
+            <tr>
+                <?php for($i = 0; $i <= count($square) -1; $i++): ?>
+                    <td><?php print $square[$i]; ?></td>
+                <?php endfor; ?>
+            </tr>
         <?php endforeach; ?>
-    </article>
+        </tbody>
+    </table>
 </body>
 </html>
