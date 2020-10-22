05cc7595aed2aa841511b2a0886e0ea3e49a3b50
Liudas Bagdonas <liudasbagd@gmail.com>
2020-10-22 11:17:42 +0300

    led colors matrix

--- index.php
+++ index.php
@@ -1,25 +1,22 @@
 <?php
-$tracks = [
-    [
-        'available' => true,
-        'pins' => [
-            [true],
-            [false, false],
-            [false, false, false],
-            [true, true, true, true]
-        ],
-    ],
-    [
-        'available' => false,
-        'pins' => [
-            [false],
-            [false, false],
-            [false, false, false],
-            [false, false, false, false]
-        ],
-    ],
+define('G', 1);
+define('Y', 2);
+define('R', 3);
+
+$green = [G];
+$yellow = [Y];
+$red = [R];
+
+$colors = [
+    [G, G, G, G, G, G, G, G],
+    [G, Y, Y, Y, Y, Y, Y, G],
+    [G, Y, R, R, R, R, Y, G],
+    [G, Y, R, Y, Y, R, Y, G],
+    [G, Y, R, Y, Y, R, Y, G],
+    [G, Y, R, R, R, R, Y, G],
+    [G, Y, Y, Y, Y, Y, Y, G],
+    [G, G, G, G, G, G, G, G],
 ];
-var_dump($tracks);
 ?>
 <html lang="en">
 <head>
