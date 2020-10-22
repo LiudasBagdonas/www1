ed51be6385f31d0e0c81793410f7c635141f03a9
Liudas Bagdonas <liudasbagd@gmail.com>
2020-10-22 13:41:20 +0300

    random generated text

--- index.php
+++ index.php
@@ -1,53 +1,15 @@
 <?php
-$my_memories = [
-    'Baras',
-    'First beer',
-    'Second beer',
-    'Third beer',
-    'Fourth beer',
-    '2',
-    '2',
-    'Fifth beer',
-    'Sixth beer',
-    'Seventh beer',
-    'Eighth beer',
-    'Ninth beer',
-    'Taxi',
-
+$words = [
+    'Penis','ananasas','Aidas', 'kiausai','alus','baras', 'triusis', 'bega','kukulis','papai','niekina',
 ];
+$text = [];
+$esey = '';
+$words_amount = rand(5,30);
 
-$friend_memories = [
-    'Baras',
-    'First Cider',
-    'Second beer',
-    '2',
-    'Third Cider',
-    '2',
-    'Fourth beer',
-    'Fifth Cider',
-    'Sixth Cider',
-    'Seventh beer',
-    'Eighth Cider',
-    'Ninth beer',
-    'Blackout',
-];
-
-$common_memories = [];
-
-foreach($my_memories as $memorie) {
-
-   if(in_array($memorie, $friend_memories) && !in_array($memorie, $common_memories)){
-       $common_memories[] = $memorie;
-   }
-
+for($i = 1; $i <= $words_amount; $i++){
+    $rand_word = rand(0, count($words) -1);
+    $esey .= $words[$rand_word] . ' ';
 }
-
-var_dump($common_memories);
-
-$fb_index = rand(0, count($my_memories) -1);
-$fb_text = $my_memories[$fb_index];
-$h3_my = "Flashback ($fb_index): $fb_text";
-
 ?>
 <html lang="en">
 <head>
@@ -57,19 +19,9 @@ $h3_my = "Flashback ($fb_index): $fb_text";
     </style>
 </head>
 <body>
-    <h1>Kas buvo penktadieni?</h1>
-    <h2>Mano prisiminimai</h2>
-    <ul>
-        <?php foreach($my_memories as $memorie): ?>
-        <li><?php print $memorie; ?></li>
-        <?php endforeach; ?>
-    </ul>
-    <h3><?php print $h3_my; ?></h3>
-    <h2>Draugo prisiminimai</h2>
-    <ul>
-        <?php foreach($friend_memories as $memorie): ?>
-            <li><?php print $memorie; ?></li>
-        <?php endforeach; ?>
-    </ul>
+    <h1>Lietuviu Egzaminas</h1>
+    <p>
+            <?php print $esey; ?>
+    </p>
 </body>
 </html>
