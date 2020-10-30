c1b750e75a1e5ca13c28c46a4e58e96db1983a2a
Liudas Bagdonas <liudasbagd@gmail.com>
2020-10-30 12:21:10 +0200

    references and cycles

--- index.php
+++ index.php
@@ -1,47 +1,41 @@
 <?php
-$size = 3;
-$array = [
-    'stringas',
-    'vardas' => [
-        'stringas',
-        'dar viena verte' => [
-            1,
-            2,
-            4,
-            'random' => [
-                'veikia?'
-            ]
-        ]
-    ],
-    1000
-];
+//$gatve = [];
+$letters = ['g', 's', 'r', 'p', 'F', 'r', 'R', 'Q',];
+$value = 'r';
+$to = '8';
 
-function print_array($array) {
-    $string = '';
+/**
+ * @param array $letters
+ * @param string $value
+ * @return int
+ */
+function count_values($letters, $value) {
+    $count = 0;
+    foreach ($letters as $letter) {
+        if ($letter === $value) {$count += 1;}
+    }
+    return $count;
+}
 
-    foreach ($array as $index => $value) {
-        if (substr($string, -1) === '.') {
-            $string = substr_replace($string, ', ', -1);
-        }
+$result = count_values($letters, $value);
+var_dump($result);
 
-        if (!is_array($value)) {
-            $string .= "$index: $value.";
-        } else {
-            $string .= "$index: " . print_array($value);
+function change_values(&$letters, $value, $to) {
+    foreach ($letters as &$letter) {
+        if ($letter === $value) {
+            $letter = $to;
         }
     }
-        
-    return $string;
 }
-$array_formated = print_array($array);
-var_dump($array_formated);
+change_values($letters, $value, $to);
+var_dump($letters);
 
 
 ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Foreach ciklas</title>
+    <title>References</title>
     <link rel="stylesheet" href="style.css?<?php print time(); ?>">
 </head>
 <body>
